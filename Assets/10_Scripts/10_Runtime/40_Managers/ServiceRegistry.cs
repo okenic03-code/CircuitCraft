04b2d1f6b23a07623f1e9e190b73e1f7ea957998
okenic03-code <okenic03@gmail.com>
2026-02-21 00:04:58 +0900

    refactor(managers): add generic API to ServiceRegistry

--- Assets/10_Scripts/10_Runtime/40_Managers/ServiceRegistry.cs
+++ Assets/10_Scripts/10_Runtime/40_Managers/ServiceRegistry.cs
@@ -1,30 +1,71 @@
+using System;
+using System.Collections.Generic;
+
 namespace CircuitCraft.Managers
 {
     public static class ServiceRegistry
     {
-        public static GameManager GameManager { get; private set; }
-        public static SimulationManager SimulationManager { get; private set; }
+        private static readonly Dictionary<Type, object> _services = new Dictionary<Type, object>();
+
+        public static GameManager GameManager => Resolve<GameManager>();
+        public static SimulationManager SimulationManager => Resolve<SimulationManager>();
+
+        /// <summary>
+        /// Registers a service instance. Prefer calling in Awake.
+        /// </summary>
+        public static void Register<T>(T service) where T : class
+        {
+            Type serviceType = typeof(T);
+
+            if (service == null)
+            {
+                _services.Remove(serviceType);
+                return;
+            }
+
+            _services[serviceType] = service;
+        }
+
+        /// <summary>
+        /// Resolves a previously registered service. Prefer calling in Start or later.
+        /// </summary>
+        public static T Resolve<T>() where T : class
+        {
+            if (_services.TryGetValue(typeof(T), out object service))
+            {
+                return service as T;
+            }
+
+            return null;
+        }
+
+        public static void Unregister<T>(T service) where T : class
+        {
+            Type serviceType = typeof(T);
+            if (_services.TryGetValue(serviceType, out object registeredService) && ReferenceEquals(registeredService, service))
+            {
+                _services.Remove(serviceType);
+            }
+        }
 
         public static void Register(GameManager gameManager)
         {
-            GameManager = gameManager;
+            Register<GameManager>(gameManager);
         }
 
         public static void Register(SimulationManager simulationManager)
         {
-            SimulationManager = simulationManager;
+            Register<SimulationManager>(simulationManager);
         }
 
         public static void Unregister(GameManager gameManager)
         {
-            if (GameManager == gameManager)
-                GameManager = null;
+            Unregister<GameManager>(gameManager);
         }
 
         public static void Unregister(SimulationManager simulationManager)
         {
-            if (SimulationManager == simulationManager)
-                SimulationManager = null;
+            Unregister<SimulationManager>(simulationManager);
         }
     }
 }
